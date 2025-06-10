ce360c365625cb9793378c0691f9e4ca9810a707
AppPerfect <appperfect@AppPerfects-Mac-mini.local>
2025-06-10 16:00:45 +0530

    mcq form added

--- resources/views/add-quiz.blade.php
+++ resources/views/add-quiz.blade.php
@@ -32,41 +32,42 @@
             @else
             <span class="text-green-500 font-bold">Quiz: {{session('quizDetails')->name}}</span>
             <h2 class="text-2xl text-center text-gray-800 mb-6 ">Add MCQ's</h2>
-            <form action="" method="get" class="space-y-4">
+            <form action="add-mcq" method="post" class="space-y-4">
+                @csrf
                 <div>
-                    <textarea type="text" placeholder="Enter your question Name" name="quiz" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none"></textarea>
+                    <textarea type="text" placeholder="Enter your question Name" name="question" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none"></textarea>
                     
                 </div>
                 <div>
-                    <input type="text" placeholder="Enter first option" name="quiz" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                    <input type="text" placeholder="Enter first option" name="a" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
                     
                 </div>
                 <div>
-                    <input type="text" placeholder="Enter second option" name="quiz" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                    <input type="text" placeholder="Enter second option" name="b" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
                     
                 </div>
                 <div>
-                    <input type="text" placeholder="Enter third option" name="quiz" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                    <input type="text" placeholder="Enter third option" name="c" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
                     
                 </div>
                 <div>
-                    <input type="text" placeholder="Enter forth option" name="quiz" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                    <input type="text" placeholder="Enter forth option" name="d" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
                     
                 </div>
 
                 <div>
-                    <select name="right answer" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
+                    <select name="correct_ans" class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none">
                         <option value="">Select Right Answer</option>
-                        <option value="">A</option>
-                        <option value="">B</option>
-                        <option value="">C</option>
-                        <option value="">D</option>
+                        <option value="a">A</option>
+                        <option value="b">B</option>
+                        <option value="c">C</option>
+                        <option value="d">D</option>
 
                     </select>
                 </div>
 
-                <button type="submit" class="w-full bg-blue-500 rounded-xl px-4 py-2 cursor-pointer text-white">Add More</button>
-                <button type="submit" class="w-full bg-green-500 rounded-xl px-4 py-2 cursor-pointer text-white">Add and Submit</button>
+                <button type="submit" name="submit" value="add-more" class="w-full bg-blue-500 rounded-xl px-4 py-2 cursor-pointer text-white">Add More</button>
+                <button type="submit" name="submit" value="done" class="w-full bg-green-500 rounded-xl px-4 py-2 cursor-pointer text-white">Add and Submit</button>
 
 
             </form>
